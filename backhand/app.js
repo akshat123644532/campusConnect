bd24140529a88ac6da9ba650ae036452aa338e54
akshat123644532 <akshatsolanki106@gmail.com>
2026-04-30 13:49:32 +0530

    fix route naming to lowercase event

--- backhand/app.js
+++ backhand/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/user");
-const eventRoutes = require("./routes/Event");
+const eventRoutes = require("./routes/event");
 
 const app = express();
 const port = process.env.PORT || 5000;
