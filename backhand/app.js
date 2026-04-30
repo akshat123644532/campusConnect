7f314ec0a21ee5bb107fa79560f401757c64d97b
akshat123644532 <akshatsolanki106@gmail.com>
2026-04-30 13:14:37 +0530

    fix event model issue

--- backhand/app.js
+++ backhand/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/user");
-const eventRoutes = require("./routes/event");
+const eventRoutes = require("./routes/Event");
 
 const app = express();
 const port = process.env.PORT || 5000;
